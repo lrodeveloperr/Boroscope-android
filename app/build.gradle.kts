687fcf9a200a3766e3ca6fde8d5110d905078dba
lrodeveloperr <lrodeveloperr@gmail.com>
2026-09-06 19:06:37 -0400

    Align compile SDK with available Android 36 platform

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.worksbien.borescopedirect"
-    compileSdk = 37
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.worksbien.borescopedirect"
