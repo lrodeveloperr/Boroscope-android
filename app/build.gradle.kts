0afe0f1c85bfde511941eea560e0b3f24cb41a0e
lrodeveloperr <lrodeveloperr@gmail.com>
2026-09-06 19:14:29 -0400

    Compile app against Android 17

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.worksbien.borescopedirect"
-    compileSdk = 36
+    compileSdk = 37
 
     defaultConfig {
         applicationId = "com.worksbien.borescopedirect"
